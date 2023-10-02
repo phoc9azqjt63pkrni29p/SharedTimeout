4c5c3f5b33b3cf5718d3d55213ef42d63f0abe79
phoc9azqjt63pkrni29p <144279683+phoc9azqjt63pkrni29p@users.noreply.github.com>
2023-10-02 23:46:12 +0300

    fix: implicit cast to seconds now return total seconds
    new: explicitly expose remaining time in a number of forms
    doc: add xml comments to public members

--- SharedTimeout/SharedTimeout.cs
+++ SharedTimeout/SharedTimeout.cs
@@ -3,34 +3,81 @@ using System.Threading;
 
 namespace SharedTimeout
 {
+    /// <summary>
+    /// Represents reusable timeout that can be implicitly casted to TimeSpan int or CancellationToken
+    /// </summary>
     public class SharedTimeout : IDisposable
     {
+        /// <summary>
+        /// Returns a <see cref="SharedTimeout"/> for specified <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="value">Time interval</param>
+        /// <returns>An object that represents value.</returns>
         public static SharedTimeout FromTimeSpan(TimeSpan value) => new SharedTimeout(value);
+        /// <summary>
+        /// Returns a <see cref="SharedTimeout"/> for specified number of milliseconds.
+        /// </summary>
+        /// <param name="value">A number of milliseconds</param>
+        /// <returns>An object that represents value.</returns>
         public static SharedTimeout FromMilliseconds(int value) => new SharedTimeout(TimeSpan.FromMilliseconds(value));
+        /// <summary>
+        /// Returns a <see cref="SharedTimeout"/> for specified number of seconds.
+        /// </summary>
+        /// <param name="value">A number of seconds</param>
+        /// <returns>An object that represents value.</returns>
         public static SharedTimeout FromSeconds(int value) => new SharedTimeout(TimeSpan.FromSeconds(value));
+        /// <summary>
+        /// Returns a <see cref="SharedTimeout"/> for specified number of minutes.
+        /// </summary>
+        /// <param name="value">A number of minutes</param>
+        /// <returns>An object that represents value.</returns>
         public static SharedTimeout FromMinutes(int value) => new SharedTimeout(TimeSpan.FromMinutes(value));
+        /// <summary>
+        /// Returns a <see cref="SharedTimeout"/> for specified number of hours.
+        /// </summary>
+        /// <param name="value">A number of hours</param>
+        /// <returns>An object that represents value.</returns>
         public static SharedTimeout FromHours(int value) => new SharedTimeout(TimeSpan.FromHours(value));
+        /// <summary>
+        /// Returns a <see cref="SharedTimeout"/> for specified number of days.
+        /// </summary>
+        /// <param name="value">A number of days</param>
+        /// <returns>An object that represents value.</returns>
         public static SharedTimeout FromDays(int value) => new SharedTimeout(TimeSpan.FromDays(value));
-
-        public static implicit operator TimeSpan(SharedTimeout timeout) => timeout.CreatedAt + timeout.Value - DateTime.Now;
-        public static implicit operator int(SharedTimeout timeout) => (timeout.CreatedAt + timeout.Value - DateTime.Now).Seconds;
+        /// <summary> Implicitly converts <paramref name="timeout"/> to remaining time interval </summary>
+        public static implicit operator TimeSpan(SharedTimeout timeout) => timeout.Value;
+        /// <summary> Implicitly converts <paramref name="timeout"/> to remaining number of seconds </summary>
+        public static implicit operator int(SharedTimeout timeout) => (int)timeout.Value.TotalSeconds;
+        /// <summary> Implicitly converts <paramref name="timeout"/> to <see cref="CancellationToken"/> which will be cancelled after remaining time interval </summary>
         public static implicit operator CancellationToken(SharedTimeout timeout)
         {
             if (timeout.Cts == null)
-                timeout.Cts = new CancellationTokenSource(timeout.CreatedAt + timeout.Value - DateTime.Now);
+                timeout.Cts = new CancellationTokenSource(timeout.Value);
             return timeout.Cts.Token;
         }
+        /// <summary> Explicit remaining time </summary>
+        public TimeSpan Value => CreatedAt + _value - DateTime.Now;
+        /// <summary> Explicit remaining time in milliseconds </summary>
+        public int ValueMilliseconds => (int)Value.TotalMilliseconds;
+        /// <summary> Explicit remaining time in seconds </summary>
+        public int ValueSeconds => (int)Value.TotalSeconds;
+        /// <summary> Explicit remaining time in minutes </summary>
+        public int ValueMinutes => (int)Value.TotalMinutes;
+        /// <summary> Explicit remaining time in hours </summary>
+        public int ValueHours => (int)Value.TotalHours;
+        /// <summary> Explicit remaining time in days </summary>
+        public int ValueDays => (int)Value.TotalDays;
 
         private SharedTimeout(TimeSpan value)
         {
             CreatedAt = DateTime.Now;
-            Value = value;
+            _value = value;
         }
 
         private SharedTimeout() : this(TimeSpan.Zero) { }
 
         private DateTime CreatedAt { get; }
-        private TimeSpan Value { get; }
+
         private CancellationTokenSource Cts { get; set; }
 
         #region Dispose
@@ -54,6 +101,9 @@ namespace SharedTimeout
         }
 
         private bool disposedValue;
+
         #endregion
+
+        private readonly TimeSpan _value;
     }
 }
